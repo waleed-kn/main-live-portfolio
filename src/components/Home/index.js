aa103668a70dc4e058ae1db27b9e02326f3c1d51
MUHAMMAD WALEED <furiouswaleed.99@gmail.com>
2026-05-03 23:42:14 +0500

    updatee

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -36,7 +36,7 @@ const Home = () => {
             <br />
             <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={15} />
           </h1>
-          <h2>Web Developer & , 2026 at City University</h2>
+          <h2>Web Developer & 2026 at City University</h2>
           <Link to="/contact" className="flat-button">CONTACT ME</Link>
         </div>
         <Logo className="Home" />
