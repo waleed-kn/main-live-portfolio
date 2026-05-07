dead9ecc42f5ffeac3215cae34836531f0b35225
MUHAMMAD WALEED <furiouswaleed.99@gmail.com>
2026-05-07 23:23:48 +0500

    fix npm build

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,16 +1,18 @@
-import './index.scss'; 
+import './index.scss';
 import Sidebar from '../Sidebar'
 import { Outlet } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 
-const Layout = () => { 
-    return(
-        <div className = "App">
+const Layout = () => {
+    return (
+        <div className="App">
             <Sidebar />
-            <div className = "page">
+            <div className="page">
                 <Outlet />
             </div>
+            <Analytics />
         </div>
-    ) 
+    )
 }
 
 export default Layout
